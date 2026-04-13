63e7cf9a4452861457363a6dc5ac6f18c4a04395
JUSSARA APARECIDA GOMES DE OLIVEIRA <jussara.agoliveira@senacsp.edu.br>
2026-04-13 17:09:26 -0300

    TelaDeServiços e TelaDePedidos

---
+++ LogiN/Pedidos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogiN
+{
+    public partial class TelaPedidos : Form
+    {
+        public TelaPedidos()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
